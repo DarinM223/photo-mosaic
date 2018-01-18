903dc9ae8b9d7d0db2ecd91d22fd6bd3caf2da32
Darin Minamoto <darinm223@gmail.com>
2018-01-17 17:03:06 -0800

    Starts nearest neighbor implementation

--- src/KDTree.hs
+++ src/KDTree.hs
@@ -10,6 +10,7 @@ import Data.List (foldl')
 
 class Dimensional d where
     atDim :: Int -> d -> Int
+    dist  :: d -> d -> Int
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
 
@@ -23,6 +24,9 @@ bulkInsert elems tree = foldl' (flip insert) tree elems
 insert :: (Dimensional a, Eq a) => a -> Tree a -> Tree a
 insert = insertInTree 0
 
+incDim :: Int -> Int
+incDim dim = dim + 1 `rem` numDims
+
 insertInTree :: (Dimensional a, Eq a) => Int -> a -> Tree a -> Tree a
 insertInTree _ e Empty = Node e Empty Empty
 insertInTree dim e (Node elem left right)
@@ -31,11 +35,36 @@ insertInTree dim e (Node elem left right)
         Node elem (insertInTree (incDim dim) e left) right
     | otherwise =
         Node elem left (insertInTree (incDim dim) e right)
+
+nearestNeighbor :: (Dimensional a, Eq a) => a -> Tree a -> Maybe a
+nearestNeighbor = nearestNeighborInTree 0
+
+nearestNeighborInTree :: (Dimensional a, Eq a) => Int -> a -> Tree a -> Maybe a
+nearestNeighborInTree _ _ Empty = Nothing
+nearestNeighborInTree _ _ (Node e Empty Empty) = Just e
+nearestNeighborInTree dim searchP (Node currP left right)
+    | searchPDim < currPDim = go left right
+    | otherwise             = go right left
   where
-    incDim dim = dim + 1 `rem` numDims
+    searchPDim = atDim dim searchP
+    currPDim = atDim dim currP
+    go t1 t2 = Just $
+        if sphereIntersectsPlane closestT1
+            then bestPoint closestT1 . searchTree $ t2
+            else closestT1
+      where
+        closestT1 = bestPoint currP . searchTree $ t1
 
-nearestNeighbor :: a -> Tree a -> Maybe a
-nearestNeighbor = nearestNeighborInTree 0 $ Rectangle (0, 0) (0, 0)
+        -- Searches and returns the nearest neighbor.
+        searchTree = nearestNeighborInTree (incDim dim) searchP
 
-nearestNeighborInTree :: Int -> Rectangle -> a -> Tree a -> Maybe a
-nearestNeighborInTree _ _ _ Empty = Nothing
+        -- Compares the nearest neighbor result to another point
+        -- by comparing their distance to the search point.
+        bestPoint compare Nothing = compare
+        bestPoint compare (Just best)
+            | dist best searchP < dist compare searchP = best
+            | otherwise                                = compare
+
+        -- TODO(DarinM223): understand how this works better
+        sphereIntersectsPlane closest =
+            (searchPDim - currPDim) ^ 2 <= dist currP closest
