542c0a666d73c0c5c1f963608eb86b67f2b99d57
Darin Minamoto <darinm223@gmail.com>
2018-01-19 02:57:10 -0800

    Modifies calcMosaic to return list of filenames

--- src/Mosaic/MosaicTask.hs
+++ src/Mosaic/MosaicTask.hs
@@ -16,11 +16,15 @@ import Control.Concurrent.STM
     )
 import Control.Monad (forM_)
 import Control.Monad.Identity (runIdentity)
+import Data.List (foldl')
 import Data.Maybe (catMaybes)
-import Mosaic.KDTree (bulkInitTree, nearestNeighbor, Dimensional (..), Tree (..))
-import Mosaic.Iterator (createRangeIter, runIteratorT, ImageIterator (imageStartPos))
+import Data.Traversable (forM)
+import Data.Vector ((//))
+import Mosaic.KDTree (bulkInitTree, nearestNeighbor, Dimensional (..), Tree)
+import Mosaic.Iterator (createRangeIter, runIteratorT, ImageIterator)
 import Mosaic.Parser (parseInt, parseSpaces, parseQuotes, runParser)
 
+import qualified Data.Vector as V
 import qualified Mosaic.AvgColor as Avg
 
 data CalcResult = CalcResult
@@ -28,10 +32,11 @@ data CalcResult = CalcResult
     , avgColor :: (Int, Int, Int)
     } deriving (Show, Eq)
 
-data ImageResult
-    = ImagePiece String (Int, Int)
-    | NewLine
-    deriving (Show, Eq)
+data ImageResult = ImageResult
+    { imageResultWidth     :: Int
+    , imageResultHeight    :: Int
+    , imageResultFilenames :: [Maybe String]
+    } deriving (Show, Eq)
 
 instance Dimensional CalcResult where
     atDim 0 c = first where (first, _, _) = avgColor c
@@ -55,7 +60,7 @@ breakRegions w h iw ih i = go (0, 0) []
         | otherwise                  = go (x + w, y) $ newIter (x, y):build
     newIter p = createRangeIter w h p i
 
-calcMosaic :: String -> String -> Int -> Int -> IO (Either String [ImageResult])
+calcMosaic :: String -> String -> Int -> Int -> IO (Either String ImageResult)
 calcMosaic indexPath imagePath numRows numCols = do
     avgColors <- loadFromFile indexPath
     dynMaybe <- readImage imagePath
@@ -66,26 +71,26 @@ calcMosaic indexPath imagePath numRows numCols = do
             results <- processImage tree numRows numCols $ convertRGB8 dyn
             return $ Right results
 
-processImage :: Tree CalcResult -> Int -> Int -> Image PixelRGB8 -> IO [ImageResult]
+processImage :: Tree CalcResult -> Int -> Int -> Image PixelRGB8 -> IO ImageResult
 processImage tree numRows numCols i = do
     q <- atomically newTQueue
-    forM_ regions $ \iter -> async $ processRegion q iter tree
-    forM_ regions $ \_ -> do
-        imageResult <- atomically $ readTQueue q
-        print imageResult
-    return []
+    forM_ (zip regions [0..]) $ \(iter, i) -> async $ processRegion i q iter tree
+    results <- forM regions $ \_ -> atomically $ readTQueue q
+    let v = foldl' buildVec (V.replicate (length regions) Nothing) results
+    return $ ImageResult w h $ V.toList v
   where
     w = imageWidth i `div` numCols
     h = imageHeight i `div` numRows
     regions = breakRegions w h (imageWidth i) (imageHeight i) i
+    buildVec v (i, value) = v // [(i, value)]
 
-processRegion :: TQueue (Maybe ImageResult) -> ImageIterator PixelRGB8 -> Tree CalcResult -> IO ()
-processRegion q iter tree = do
+processRegion :: Int -> TQueue (Int, Maybe String) -> ImageIterator PixelRGB8 -> Tree CalcResult -> IO ()
+processRegion i q iter tree = do
     avg <- runIteratorT (Avg.avgColor Avg.convertPixel) iter
     let avgCalcRes  = CalcResult "" avg
         nearest     = nearestNeighbor avgCalcRes tree
-        imageResult = (\r -> ImagePiece (filename r) (imageStartPos iter)) <$> nearest
-    atomically $ writeTQueue q imageResult
+        imageResult = filename <$> nearest
+    atomically $ writeTQueue q (i, imageResult)
 
 parseLine :: String -> Maybe CalcResult
 parseLine = convertToMaybe . runIdentity . runParser go
